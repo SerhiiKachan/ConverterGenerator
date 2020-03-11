3a17664b161100e7df46a516f11834dfdc8735e5
Serhii.Kachan <Serhii.Kachan@cantire.com>
2020-03-11 17:27:48 +0200

    first commit

---
+++ src/org/dudariev/converter/generator/GenerateConverterAction.java
@@ -0,0 +1,201 @@
+package org.dudariev.converter.generator;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiModifier;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
+import com.intellij.psi.codeStyle.JavaCodeStyleManager;
+import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+
+public class GenerateConverterAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        PsiClass psiClass = getPsiClassFromContext(e);
+        if (psiClass == null) {
+            return;
+        }
+        GenerateConverterDialog generateConverterDialog = new GenerateConverterDialog(psiClass);
+        generateConverterDialog.show();
+        if (generateConverterDialog.isOK()) {
+            PsiClass classTo = generateConverterDialog.getConvertToClass();
+            PsiClass classFrom = generateConverterDialog.getConvertFromClass();
+            generateConvertAs(classTo, classFrom, psiClass, generateConverterDialog.isInheritFields());
+        }
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        PsiClass psiClass = getPsiClassFromContext(e);
+        e.getPresentation().setEnabled(psiClass != null);
+    }
+
+    private PsiClass getPsiClassFromContext(AnActionEvent e) {
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        if (psiFile == null || editor == null) {
+            return null;
+        }
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement elementAt = psiFile.findElementAt(offset);
+        return PsiTreeUtil.getParentOfType(elementAt, PsiClass.class);
+    }
+
+    private void generateConvertAs(PsiClass to, PsiClass from, PsiClass psiClass, boolean useInherited) {
+        WriteCommandAction.writeCommandAction(psiClass.getProject())
+                .run(() -> buildConvertMethod(to, from, psiClass, useInherited));
+    }
+
+    private void buildConvertMethod(PsiClass to, PsiClass from, PsiClass psiClass, boolean useInherited) {
+        StringBuilder builder = buildMethodSignature(to, from);
+
+        FieldsMappingResult mappingResult = new FieldsMappingResult();
+        processToFields(to, from, mappingResult, useInherited);
+        processFromFields(from, mappingResult, useInherited);
+
+        String indentation = getProjectIndentation(psiClass);
+        builder.append(writeMappedFields(mappingResult));
+        builder.append(writeNotMappedFields(mappingResult.getNotMappedToFields(), indentation, "TO"));
+        builder.append(writeNotMappedFields(mappingResult.getNotMappedFromFields(), indentation, "FROM"));
+
+        builder.append("return to;\n}");
+
+        PsiElementFactory elementFactory = JavaPsiFacade.getElementFactory(psiClass.getProject());
+        PsiMethod convertAs = elementFactory.createMethodFromText(builder.toString(), psiClass);
+        PsiElement method = psiClass.add(convertAs);
+        JavaCodeStyleManager.getInstance(psiClass.getProject()).shortenClassReferences(method);
+    }
+
+    private void processToFields(PsiClass to, PsiClass from, FieldsMappingResult mappingResult, boolean useInherited) {
+        for (PsiField toField : getFields(to, useInherited)) {
+            String toFieldName = toField.getName();
+            if (!toField.hasModifierProperty(PsiModifier.STATIC)) {
+                PsiMethod toSetter = findSetter(to, toFieldName, useInherited);
+                PsiMethod fromGetter = findGetter(from, toFieldName, useInherited);
+                if (toSetter != null && fromGetter != null && isMatchingFieldType(toField, fromGetter)) {
+                    mappingResult.addMappedField(toSetter, fromGetter);
+                } else {
+                    mappingResult.addNotMappedToField(toFieldName);
+                }
+            }
+        }
+    }
+
+    private void processFromFields(PsiClass from, FieldsMappingResult mappingResult, boolean useInherited) {
+        for (PsiField fromField : getFields(from, useInherited)) {
+            String fromFieldName = fromField.getName();
+            if (!fromField.hasModifierProperty(PsiModifier.STATIC)) {
+                PsiMethod fromGetter = findGetter(from, fromFieldName, useInherited);
+                if (fromGetter == null || !mappingResult.getMappedFields().containsValue(fromGetter)) {
+                    mappingResult.addNotMappedFromField(fromFieldName);
+                }
+            }
+        }
+    }
+
+    @NotNull
+    private PsiField[] getFields(PsiClass clazz, boolean useInherited) {
+        PsiField[] fields;
+        if (useInherited) {
+            fields = clazz.getAllFields();
+        } else {
+            fields = clazz.getFields();
+        }
+        return fields;
+    }
+
+    @NotNull
+    private StringBuilder buildMethodSignature(PsiClass to, PsiClass from) {
+        StringBuilder builder = new StringBuilder("public ");
+        builder.append(to.getQualifiedName());
+        builder.append(" convertAs(");
+        builder.append(from.getQualifiedName());
+        builder.append(" from) {\n");
+        builder.append(to.getQualifiedName()).append(" to = new ").append(to.getQualifiedName()).append("();\n");
+        return builder;
+    }
+
+    @NotNull
+    private String writeMappedFields(FieldsMappingResult mappingResult) {
+        StringBuilder builder = new StringBuilder();
+        for (PsiMethod toSetter : mappingResult.getMappedFields().keySet()) {
+            builder.append("to.").append(toSetter.getName()).append("(from.")
+                    .append(mappingResult.getMappedFields().get(toSetter).getName()).append("());\n");
+        }
+        return builder.toString();
+    }
+
+    @NotNull
+    private String writeNotMappedFields(List<String> notMappedFields, String indentation, String sourceType) {
+        StringBuilder builder = new StringBuilder();
+        if (!notMappedFields.isEmpty()) {
+            builder.append("\n").append(indentation).append("// Not mapped ").append(sourceType).append(" fields: \n");
+        }
+        for (String notMappedField : notMappedFields) {
+            builder.append(indentation).append("// ").append(notMappedField).append("\n");
+        }
+        return builder.toString();
+    }
+
+    private PsiMethod findSetter(PsiClass psiClass, String fieldName, boolean useInherited) {
+        PsiMethod[] setters = psiClass.findMethodsByName("set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1), useInherited);
+        if (setters.length == 1) {
+            PsiMethod setter = setters[0];
+            return setter.hasModifierProperty(PsiModifier.STATIC) ? null : setter;
+        }
+        return null;
+    }
+
+    private PsiMethod findGetter(PsiClass psiClass, String fieldName, boolean useInherited) {
+        String methodSuffix = fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+        PsiMethod[] getters = psiClass.findMethodsByName("get" + methodSuffix, useInherited);
+        PsiMethod getter;
+        if (getters.length > 0) {
+            getter = getters[0];
+            return getter.hasModifierProperty(PsiModifier.STATIC) ? null : getter;
+        }
+        getters = psiClass.findMethodsByName("is" + methodSuffix, false);
+        if (getters.length > 0) {
+            getter = getters[0];
+            return getter.hasModifierProperty(PsiModifier.STATIC) ? null : getter;
+        }
+        return null;
+    }
+
+    private String getProjectIndentation(PsiClass psiClass) {
+        CommonCodeStyleSettings.IndentOptions indentOptions = CodeStyleSettings.IndentOptions.retrieveFromAssociatedDocument(psiClass.getContainingFile());
+        String indentation = "        ";
+        if (indentOptions != null) {
+            if (indentOptions.USE_TAB_CHARACTER) {
+                indentation = "\t\t";
+            } else {
+                indentation = new String(new char[2 * indentOptions.INDENT_SIZE]).replace("\0", " ");
+            }
+        }
+        return indentation;
+    }
+
+    private boolean isMatchingFieldType(PsiField toField, PsiMethod fromGetter) {
+        PsiType fromGetterReturnType = fromGetter.getReturnType();
+        PsiType toFieldType = toField.getType();
+        return fromGetterReturnType != null && toFieldType.isAssignableFrom(fromGetterReturnType);
+    }
+
+}
---
+++ src/org/dudariev/converter/generator/GenerateConverterDialog.java
@@ -0,0 +1,138 @@
+package org.dudariev.converter.generator;
+
+import com.intellij.openapi.fileEditor.impl.EditorHistoryManager;
+import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.LabeledComponent;
+import com.intellij.openapi.ui.ValidationInfo;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiShortNamesCache;
+import com.intellij.ui.TextFieldWithAutoCompletion;
+import com.intellij.util.ui.JBUI;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+
+public class GenerateConverterDialog extends DialogWrapper {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 108;
+
+    private JPanel dialog;
+    private PsiClass psiClass;
+    private TextFieldWithAutoCompletion<String> toField;
+    private TextFieldWithAutoCompletion<String> fromField;
+    private JCheckBox inheritFields;
+
+    public GenerateConverterDialog(PsiClass psiClass) {
+        super(psiClass.getProject());
+        this.psiClass = psiClass;
+        this.dialog = createConverterDialog();
+        List<String> classNamesForAutocompletion = getClassNamesForAutocompletion();
+        this.toField = createTextField(classNamesForAutocompletion);
+        this.fromField = createTextField(classNamesForAutocompletion);
+        this.inheritFields = new JCheckBox("Use inherited fields");
+
+        LabeledComponent<TextFieldWithAutoCompletion<String>> convertToComponent = LabeledComponent.create(toField, "Convert To class");
+        LabeledComponent<TextFieldWithAutoCompletion<String>> convertFromComponent = LabeledComponent.create(fromField, "Convert From class");
+
+        dialog.add(convertToComponent, BorderLayout.PAGE_START);
+        dialog.add(convertFromComponent, BorderLayout.CENTER);
+        dialog.add(this.inheritFields, BorderLayout.PAGE_END);
+
+        init();
+    }
+
+    @Nullable
+    @Override
+    protected ValidationInfo doValidate() {
+        ValidationInfo toFieldValidation = validateTextField(toField, "Target");
+        if (toFieldValidation == null) {
+            return validateTextField(fromField, "From");
+        }
+        return toFieldValidation;
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        return dialog;
+    }
+
+    private JPanel createConverterDialog() {
+        setTitle("Select Classes for Conversion");
+        JPanel dialog = new JPanel(new BorderLayout());
+        dialog.setPreferredSize(JBUI.size(WIDTH, HEIGHT));
+        dialog.setMinimumSize(JBUI.size(WIDTH, HEIGHT));
+        dialog.setMaximumSize(JBUI.size(WIDTH + 100, HEIGHT + 20));
+        return dialog;
+    }
+
+    private List<String> getClassNamesForAutocompletion() {
+        List<String> history = Stream.of(EditorHistoryManager.getInstance(psiClass.getProject()).getFiles())
+                .map(VirtualFile::getNameWithoutExtension)
+                .distinct()
+                .collect(toList());
+
+        List<String> projectFiles = FileTypeIndex.getFiles(StdFileTypes.JAVA, GlobalSearchScope.allScope(psiClass.getProject()))
+                .stream()
+                .map(VirtualFile::getNameWithoutExtension)
+                .collect(toList());
+
+        history.addAll(projectFiles);
+        return history;
+    }
+
+    private TextFieldWithAutoCompletion<String> createTextField(List<String> classNames) {
+        TextFieldWithAutoCompletion<String> textField = TextFieldWithAutoCompletion.create(psiClass.getProject(), classNames, true, null);
+        textField.setOneLineMode(true);
+        return textField;
+    }
+
+    public PsiClass getConvertToClass() {
+        return extractPsiClass(this.toField);
+    }
+
+    public PsiClass getConvertFromClass() {
+        return extractPsiClass(this.fromField);
+    }
+
+    public boolean isInheritFields() {
+        return this.inheritFields.isSelected();
+    }
+
+    private PsiClass extractPsiClass(TextFieldWithAutoCompletion<String> textField) {
+        String className = textField.getText();
+        if (className.isEmpty()) {
+            throw new IllegalArgumentException("Should select smth");
+        }
+        PsiClass[] resolvedClasses = PsiShortNamesCache.getInstance(psiClass.getProject()).getClassesByName(className, GlobalSearchScope.projectScope(psiClass.getProject()));
+        if (resolvedClasses.length == 0) {
+            throw new IllegalArgumentException("No such class found: " + className);
+        }
+        return resolvedClasses[0];
+    }
+
+    private ValidationInfo validateTextField(TextFieldWithAutoCompletion<String> textField, String fieldName) {
+        String className = textField.getText();
+        if (className.isEmpty()) {
+            return new ValidationInfo(String.format("%s class should be selected", fieldName), textField);
+        }
+        PsiClass[] resolvedClasses = PsiShortNamesCache.getInstance(psiClass.getProject()).getClassesByName(className, GlobalSearchScope.projectScope(psiClass.getProject()));
+        if (resolvedClasses.length == 0) {
+            return new ValidationInfo(String.format("Failed to find a class %s in the current project", className), textField);
+        }
+        return null;
+    }
+}
